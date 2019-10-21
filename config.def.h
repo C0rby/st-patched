bd258aeeccc8f65a1d53801fe731aa05e0e780ca
Corby <corby@posteo.de>
2019-10-21 22:32:12 +0200

    change color theme to Google-Dark

--- config.def.h
+++ config.def.h
@@ -80,46 +80,51 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#ffffff",
-	"#cc342b",
-	"#198844",
-	"#fba921",
-	"#3870ed",
-	"#a26ac7",
-	"#3870ed",
-	"#373b41",
+	[0] = "#1d1f21", /* black   */
+	[1] = "#cc342b", /* red     */
+	[2] = "#198844", /* green   */
+	[3] = "#fba922", /* yellow  */
+	[4] = "#3971ed", /* blue    */
+	[5] = "#a36ac7", /* magenta */
+	[6] = "#3971ed", /* cyan    */
+	[7] = "#c5c8c6", /* white   */
 
 	/* 8 bright colors */
-	"#c5c8c6",
-	"#cc342b",
-	"#198844",
-	"#fba921",
-	"#3870ed",
-	"#a26ac7",
-	"#3870ed",
-	"#1d1f21",
+	[8]  = "#969896", /* black   */
+	[9]  = "#cc342b", /* red     */
+	[10] = "#198844", /* green   */
+	[11] = "#fba922", /* yellow  */
+	[12] = "#3971ed", /* blue    */
+	[13] = "#a36ac7", /* magenta */
+	[14] = "#3971ed", /* cyan    */
+	[15] = "#ffffff", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	/* special colors */
+	[256] = "#1d1f21", /* background */
+	[257] = "#c5c8c6", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/*
+ *  * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ *   * will reverse too. Another logic would only make the simple feature too
+ *    * complex.
+ *     */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
